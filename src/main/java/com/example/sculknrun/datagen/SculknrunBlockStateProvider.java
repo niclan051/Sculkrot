fbf09875d81bc73b7c1cf99183e9e504feef4662
niclan051 <niclan052@gmail.com>
2024-11-25 19:15:23 +0300

    added datagen + fixed assets location

---
+++ src/main/java/com/example/sculknrun/datagen/SculknrunBlockStateProvider.java
@@ -0,0 +1,24 @@
+package com.example.sculknrun.datagen;
+
+import com.example.sculknrun.Sculknrun;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class SculknrunBlockStateProvider extends BlockStateProvider {
+    public SculknrunBlockStateProvider(PackOutput output, String modid,
+                                       ExistingFileHelper exFileHelper) {
+        super(output, modid, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        simpleBlockWithItem(
+                Sculknrun.SCULK_NODE.get(), models().cubeBottomTop(
+                        "sculk_node", modLoc("block/sculk_node_side"),
+                        modLoc("block/sculk_node_bottom"),
+                        modLoc("block/sculk_node_top")
+                )
+        );
+    }
+}
