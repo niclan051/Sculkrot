6216b40da286d7061acffe5091411bf0412f25b3
niclan051 <niclan052@gmail.com>
2024-11-27 18:23:41 +0300

    lang datagen shenanigans

--- src/main/java/com/example/sculknrun/Sculknrun.java
+++ src/main/java/com/example/sculknrun/Sculknrun.java
@@ -14,7 +14,6 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
--- src/main/java/com/example/sculknrun/datagen/ModBlockStateProvider.java
+++ src/main/java/com/example/sculknrun/datagen/ModBlockStateProvider.java
@@ -5,9 +5,9 @@ import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
-public class SculknrunBlockStateProvider extends BlockStateProvider {
-    public SculknrunBlockStateProvider(PackOutput output, String modid,
-                                       ExistingFileHelper exFileHelper) {
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, String modid,
+                                 ExistingFileHelper exFileHelper) {
         super(output, modid, exFileHelper);
     }
 
